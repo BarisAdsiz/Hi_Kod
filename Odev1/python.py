1f57162339c84c804ab929bf71158f9e568ded41
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2024-01-20 18:02:38 +0300

    Add files via upload

---
+++ Odev1/python.py
@@ -0,0 +1,44 @@
+sayi = 5
+sayi_str = "10"
+
+# String'i int'e çevirme
+sayi_str_int = int(sayi_str)
+#################################
+
+
+ondalik_sayi = 3.14
+sayi_str = "3.14"
+
+# String'i ondalıklı sayıya çevirme
+sayi_str_float = float(sayi_str)
+#################################
+
+
+sayi = 42
+ondalik_sayi = 3.14
+
+# Int'i ve float'ı string'e çevirme
+sayi_str = str(sayi)
+ondalik_sayi_str = str(ondalik_sayi)
+#################################
+
+
+sayi = 42
+liste = [1, 2, 3]
+
+# Int'i listeye çevirme
+sayi_liste = list(sayi)
+
+# Liste'yi tuple'a çevirme
+liste_tuple = tuple(liste)
+#################################
+
+
+sayi = 0
+metin = "Python"
+
+# Int'i ve string'i boolean'a çevirme
+sayi_bool = bool(sayi)
+metin_bool = bool(metin)
+#################################
+
